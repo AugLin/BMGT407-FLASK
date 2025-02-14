12d9ee8c3e3c90bfb4e8aab4691366a6f2e7c6a4
Caifu Lin <105020004+AugLin@users.noreply.github.com>
2025-02-13 20:48:43 -0500

    update

--- app.py
+++ app.py
@@ -1,16 +1,6 @@
 from flask import Flask, request, render_template, jsonify
-import git  # GitPython library
 
 app = Flask(__name__)
-
-@app.route('/git_update', methods=['POST'])
-def git_update():
-    repo = git.Repo('./lcaifu')
-    origin = repo.remotes.origin
-    repo.create_head('main',
-                     origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
-    origin.pull()
-    return '', 200
 
 @app.route('/')
 def home():
