3d25319d93f6a85de429b982005b928f74d826b2
Caifu Lin <105020004+AugLin@users.noreply.github.com>
2025-02-13 19:49:55 -0500

    add cicd

--- app.py
+++ app.py
@@ -2,6 +2,15 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+@app.route('/git_update', methods=['POST'])
+def git_update():
+    repo = git.Repo('./orbe')
+    origin = repo.remotes.origin
+    repo.create_head('main',
+                     origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+    origin.pull()
+    return '', 200
+
 @app.route('/')
 def home():
     return """
